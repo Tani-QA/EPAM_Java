6676e1e6e683658d1aebe834b8cbef813cf07c88
Tani <115032148+Tani-QA@users.noreply.github.com>
2023-08-15 18:15:43 +0300

    meet-autocode

---
+++ meet-autocode/src/main/java/MeetAStranger/MeetAStranger.java
@@ -0,0 +1,11 @@
+package MeetAStranger;
+import java.util.Scanner;
+
+public class MeetAStranger {
+    public static void main(String[] args) {
+        //ввод строки и вывод целой строки
+       Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        System.out.println("Hello, "+input);
+    }
+}
