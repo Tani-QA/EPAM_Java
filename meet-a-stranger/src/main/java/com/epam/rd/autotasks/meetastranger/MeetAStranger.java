46487143589f34a94138cfd3db62c31248ec56b9
Tani <115032148+Tani-QA@users.noreply.github.com>
2023-08-15 18:16:56 +0300

    meet-a-stranger

---
+++ meet-a-stranger/src/main/java/com/epam/rd/autotasks/meetastranger/MeetAStranger.java
@@ -0,0 +1,12 @@
+package com.epam.rd.autotasks.meetastranger;
+//package MeetAStranger;
+import java.util.Scanner;
+
+public class MeetAStranger {
+    public static void main(String[] args) {
+        //ввод строки и вывод целой строки
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        System.out.println("Hello, "+input);
+    }
+}
